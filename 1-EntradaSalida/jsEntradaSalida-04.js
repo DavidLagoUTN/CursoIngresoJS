0dd2b6af60898f9e7dd8b8878fcdd93c8cb7fa32
DavidLagoUTN <davelake56@gmail.com>
2022-02-03 17:03:15 -0300

    Ejercicios resueltos

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,9 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombreIngresado;
+	nombreIngresado =prompt("Ingresá tu nombre");
+	document.getElementById("txtIdNombre").value = nombreIngresado;
+	alert(nombreIngresado);
 }
 
